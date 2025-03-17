136d3c4e74ed40bf113431b5cc910a87a657e889
dani31perez <perezbarredad@gmail.com>
2025-03-17 15:46:27 -0600

    Integrate firebaseConfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import { Form, Row, Button, Col, Container } from "react-bootstrap";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [file, setFile] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(file);
+  }
 
   return (
-    <>
-      
-    </>
-  )
+    <Container className="mt-5">
+      <Form onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col}>
+            <Form.Label>Title</Form.Label>
+            <Form.Control type="text" placeholder="Enter title" onChange={(e) => setTitle(e.target.value)}/>
+          </Form.Group>
+
+          <Form.Group as={Col}>
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="text" placeholder="Description" onChange={(e) => setDescription(e.target.value)}/>
+          </Form.Group>
+        </Row>
+
+        <Form.Group className="mb-3" >
+          <Form.Label>Image</Form.Label>
+          <Form.Control type="file" onChange={(e) => setFile(e.target.value)}/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAa4Gs8ynyseMC7zw019bi84dHgM-TUTQE",
+  authDomain: "lab10-deabd.firebaseapp.com",
+  projectId: "lab10-deabd",
+  storageBucket: "lab10-deabd.firebasestorage.app",
+  messagingSenderId: "326149404764",
+  appId: "1:326149404764:web:e18ae9c2c67015083d8e99"
+};
+
+const app = initializeApp(firebaseConfig);
