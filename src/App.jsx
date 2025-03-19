4bf5b2d37897d5a03725152378a530f4ebde543b
dani31perez <perezbarredad@gmail.com>
2025-03-18 23:19:25 -0600

    Enable saving recipes with images in Firestore and Firebase Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
 import { Form, Row, Button, Col, Container } from "react-bootstrap";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { setDoc, doc, collection, getDocs } from "firebase/firestore";
 import "./App.css";
 import { useState } from "react";
+import { storage, db } from "./firebaseConfig";
 
 function App() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState(
+    new File([], "empty.txt", { type: "text/plain" })
+  );
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(file);
-  }
+    let refFile = "";
+
+    if (file && file.size !== 0) {
+      const fileName = file.name;
+      const storageRef = ref(storage, "images/" + fileName);
+      await uploadBytes(storageRef, file);
+      refFile = await getDownloadURL(storageRef);
+    }
+
+    const data = {
+      Title: title,
+      Description: description,
+      File: refFile || "",
+    };
+
+    const docs = await getDocs(collection(db, "Recipes"));
+    await setDoc(doc(db, "Recipes", docs.size + 1 + ""), data);
+
+    setTitle("");
+    setDescription("");
+    setFile(new File([], "empty.txt", { type: "text/plain" }));
+    e.target.reset();
+    alert("Recipe added successfully");
+  };
 
   return (
     <Container className="mt-5">
@@ -18,18 +45,31 @@ function App() {
         <Row className="mb-3">
           <Form.Group as={Col}>
             <Form.Label>Title</Form.Label>
-            <Form.Control type="text" placeholder="Enter title" onChange={(e) => setTitle(e.target.value)}/>
+            <Form.Control
+              type="text"
+              placeholder="Enter title"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
           </Form.Group>
 
           <Form.Group as={Col}>
             <Form.Label>Description</Form.Label>
-            <Form.Control type="text" placeholder="Description" onChange={(e) => setDescription(e.target.value)}/>
+            <Form.Control
+              type="text"
+              placeholder="Description"
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+            />
           </Form.Group>
         </Row>
 
-        <Form.Group className="mb-3" >
+        <Form.Group className="mb-3">
           <Form.Label>Image</Form.Label>
-          <Form.Control type="file" onChange={(e) => setFile(e.target.value)}/>
+          <Form.Control
+            type="file"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAa4Gs8ynyseMC7zw019bi84dHgM-TUTQE",
@@ -6,7 +8,9 @@ const firebaseConfig = {
   projectId: "lab10-deabd",
   storageBucket: "lab10-deabd.firebasestorage.app",
   messagingSenderId: "326149404764",
-  appId: "1:326149404764:web:e18ae9c2c67015083d8e99"
+  appId: "1:326149404764:web:e18ae9c2c67015083d8e99",
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
