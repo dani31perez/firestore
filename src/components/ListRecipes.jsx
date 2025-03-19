14792a37f2e5645f0239cf38144fe1c967655d95
dani31perez <perezbarredad@gmail.com>
2025-03-19 01:57:01 -0600

    Add functionality to edit recipes in Firestore

--- src/components/ListRecipes.jsx
+++ src/components/ListRecipes.jsx
@@ -1,12 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { doc, collection, onSnapshot, deleteDoc } from "firebase/firestore";
+import {
+  doc,
+  collection,
+  onSnapshot,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../firebaseConfig";
-import { Col, Container, ListGroup, Row } from "react-bootstrap";
+import {
+  Col,
+  Container,
+  FloatingLabel,
+  Form,
+  InputGroup,
+  ListGroup,
+  Row,
+} from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFloppyDisk,
+  faPenToSquare,
+  faTrash,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 function ListRecipes() {
   const [recipes, setRecipes] = useState([]);
+  const [editTitle, setEditTitle] = useState("");
+  const [editDescription, setEditDescription] = useState("");
+  const [editRecipeId, setEditRecipeId] = useState(null);
 
   useEffect(() => {
     const colRef = collection(db, "Recipes");
@@ -21,11 +43,35 @@ function ListRecipes() {
     return () => unsubscribe();
   }, []);
 
+  const handleEdit = (recipe) => {
+    setEditDescription(recipe.Description);
+    setEditTitle(recipe.Title);
+    setEditRecipeId(recipe.Id);
+    console.log(editRecipeId);
+  };
+
+  const handleSave = async (recipeId) => {
+    try {
+      const recipeRef = doc(db, "Recipes", recipeId);
+      await updateDoc(recipeRef, {
+        Title: editTitle,
+        Description: editDescription,
+      });
+      setEditRecipeId(null);
+    } catch (err) {
+      console.error("Error updating recipe:", err);
+    }
+  };
+
+  const handleCancel = () => {
+    setEditRecipeId(null);
+  };
+
   const handleDelete = async (recipeId) => {
     try {
       await deleteDoc(doc(db, "Recipes", recipeId));
     } catch (err) {
-      console.error("Error deleting product:", err);
+      console.error("Error deleting recipe:", err);
     }
   };
 
@@ -33,21 +79,65 @@ function ListRecipes() {
     <ListGroup>
       {recipes.map((recipe) => (
         <ListGroup.Item key={recipe.Id} className="border-0 mt-3">
-          <Container>
-            <Row>
-              <Col>
-                <h4>{recipe.Title}</h4>
-                <p className="fs-5">{recipe.Description}</p>
-              </Col>
-              <Col>
-                <FontAwesomeIcon icon={faPenToSquare} className="me-5 fs-1" />
-                <FontAwesomeIcon
-                  icon={faTrash}
-                  className="fs-1"
-                  onClick={() => handleDelete(recipe.Id)}
-                />
-              </Col>
-            </Row>
+          <Container
+            className="align-items-center"
+            style={{ justifyItems: "center" }}
+          >
+            {editRecipeId === recipe.Id ? (
+              <Row className="align-items-center" style={{ width: "70vw" }}>
+                <Col md={8} xs={6}>
+                  <InputGroup>
+                    <FloatingLabel label="Title" className="me-3">
+                      <Form.Control
+                        placeholder="title"
+                        type="text"
+                        value={editTitle}
+                        onChange={(e) => setEditTitle(e.target.value)}
+                      />
+                    </FloatingLabel>
+                    <FloatingLabel label="Description">
+                      <Form.Control
+                        type="text"
+                        placeholder="description"
+                        value={editDescription}
+                        onChange={(e) => setEditDescription(e.target.value)}
+                      />
+                    </FloatingLabel>
+                  </InputGroup>
+                </Col>
+                <Col md="auto" xs="auto">
+                  <FontAwesomeIcon
+                    icon={faFloppyDisk}
+                    className="me-3 fs-1"
+                    onClick={() => handleSave(recipe.Id)}
+                  />
+                  <FontAwesomeIcon
+                    icon={faXmark}
+                    className="fs-1"
+                    onClick={() => handleCancel()}
+                  />
+                </Col>
+              </Row>
+            ) : (
+              <Row className="align-items-center" style={{ width: "70vw" }}>
+                <Col md={8} xs={6}>
+                  <h4>{recipe.Title}</h4>
+                  <p className="fs-5">{recipe.Description}</p>
+                </Col>
+                <Col md="auto" xs="auto">
+                  <FontAwesomeIcon
+                    icon={faPenToSquare}
+                    className="me-3 fs-1"
+                    onClick={() => handleEdit(recipe)}
+                  />
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    className="fs-1"
+                    onClick={() => handleDelete(recipe.Id)}
+                  />
+                </Col>
+              </Row>
+            )}
           </Container>
         </ListGroup.Item>
       ))}
